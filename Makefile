8b440128510b1fc8b164452bb87791a829795a94
Abinaya Mahendiran <abinaya.m02@mphasis.com>
2021-04-25 09:04:52 +0000

    Removed os path setting - used export instead

--- Makefile
+++ Makefile
@@ -4,3 +4,8 @@ style:
 	black .
 	flake8
 	isort .
+
+# Tests
+.PHONY: test
+test:
+	pytest --cov scripts --cov-report html
--- tests/annotation/test_annotator.py
+++ tests/annotation/test_annotator.py
@@ -1,5 +1,5 @@
-import os
-import sys
+# import os
+# import sys
 
 import numpy as np
 import pandas as pd
@@ -11,9 +11,9 @@ from scripts.annotator import (  # random_sampling,
     margin_sampling,
 )
 
-PACKAGE_PARENT = ".."
-SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd())))
-sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
+# PACKAGE_PARENT = ".."
+# SCRIPT_DIR = os.path.dirname(os.path.realpath(os.path.join(os.getcwd())))
+# sys.path.append(os.path.normpath(os.path.join(SCRIPT_DIR, PACKAGE_PARENT)))
 
 
 @pytest.fixture
